c2e2bbb350a97ce0211fd07d6488b4f8e37f6952
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-04 20:47:13 -0300

    refactor: replace string concatenation with template literals in even/odd logic

--- js-exercises/01-par-ou-impar.js
+++ js-exercises/01-par-ou-impar.js
@@ -7,7 +7,7 @@ let number = Number(prompt("Insira um número para verificar se ele é PAR ou Í
 if(isNaN(number)){
     console.log('Erro: Digite um número válido, por favor!')
 }else if(number % 2 === 0){
-    console.log('O número ' + number + ' é PAR!')
+    console.log(`O número ${number} é PAR!`)
 }else{
-    console.log('O número ' + number + ' é ÍMPAR!')
+    console.log(`O número ${number} é ÍMPAR!`)
 }
