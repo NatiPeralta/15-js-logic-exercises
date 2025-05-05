9cb7bcf3292d7e174a15b05d05c64cc5f8e1e786
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-04 23:05:07 -0300

    feat: add calculadora-imc exercise

--- js-exercises/05-calculadora-imc.js
+++ js-exercises/05-calculadora-imc.js
@@ -1,1 +1,23 @@
 //5. Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade) utilizando if-else.
+//IMC peso (kg) / (altura (m) * altura (m))
+
+const prompt = require('prompt-sync')()
+
+let altura = parseFloat(prompt('Por favor, digite a sua altura: ').replace(',', '.'))
+let peso = parseFloat(prompt('Por favor, digite o seu peso: ').replace(',', '.'))
+
+let IMC = peso/(altura * altura)
+
+console.log(`O seu IMC é ${IMC.toFixed(2)}`)
+
+if(IMC < 18.5){
+    console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Baixo peso!`)
+}else if(IMC < 24.9){
+    console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Peso normal!`)
+}else if(IMC < 29.9){
+    console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Sobrepeso!`)
+}else if(IMC < 39.9){
+    console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Obesidade!`)
+}else{
+    console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Obesidade grave!`)
+}
---
+++ js-exercises/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+if(IMC < 18,5){
+//     console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Baixo peso!`)
+// }else if(IMC >= 18,5 && IMC <= 24,9){
+//     console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Peso normal!`)
+// }else if(IMC >= 25 && IMC <= 29,9){
+//     console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Sobrepeso!`)
+// }else if(IMC >= 30 && IMC <= 39,9){
+//     console.log(`O seu Índice de Massa Corporal (IMC) está na categoria: Obesidade!`)
+// }else{
+//     console.log(`Erro: Por favor, insira um peso e altura válidos!`)
+// }
