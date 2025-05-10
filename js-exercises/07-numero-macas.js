60a5d00115c8977010e1fe700b792a7b7e37c789
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 17:58:48 -0300

    feat: add numero-macas exercise

--- js-exercises/07-numero-macas.js
+++ js-exercises/07-numero-macas.js
@@ -1,1 +1,16 @@
 //7. As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+
+const prompt = require('prompt-sync')()
+
+let qtdMacas = Number(prompt('Quantas maçãs você comprou? '))
+let totCompra = 0
+
+if(isNaN(qtdMacas) || qtdMacas <= 0){
+    console.log('Erro: Por favor digite um número válido e positivo para realizar o cálculo da sua compra de maçãs!')
+}else if(qtdMacas >= 12){
+    totCompra = (qtdMacas*0.25)
+    console.log(`Total da sua compra de ${qtdMacas} maçãs com desconto: R$ ${totCompra.toFixed(2)}`)
+}else{
+    totCompra = (qtdMacas*0.30)
+    console.log(`Total da sua compra de ${qtdMacas} maçãs sem desconto: R$ ${totCompra.toFixed(2)}`)
+}
