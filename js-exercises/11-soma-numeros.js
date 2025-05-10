a530c5511a08379c44faf6541679960fb59898fb
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 20:58:43 -0300

    feat: add soma-numeros exercise

--- js-exercises/11-soma-numeros.js
+++ js-exercises/11-soma-numeros.js
@@ -1,1 +1,13 @@
 //11. Escreva um programa que solicita ao usuário 5 números e calcula a soma total utilizando um loop for.
+
+const prompt = require('prompt-sync')()
+let soma = 0
+let numUser = [] //array para guardar os números
+
+for(let numb = 1; numb <= 5; numb++){
+    let number = Number(prompt('Digite um número: '))
+    numUser.push(number);  //adiciona o número ao array
+    soma += number
+}
+console.log(`Os números digitados foram: ${numUser.join(', ')}`); //transforma o array em uma string separada por vírgulas
+console.log(`A soma dos números é: ${soma}`)
--- js-exercises/12-tabuada.js
+++ js-exercises/12-tabuada.js
@@ -1,1 +1,14 @@
 //12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a 10) utilizando um loop for.
+
+const prompt = require('prompt-sync')()
+
+let number = Number(prompt('Digite um número de 1 a 10 para exibir a tabuada: '))
+
+if(number >= 1 && number <= 10){
+    console.log(`A tabuada do número ${number} é: `)
+    for(let i = 1; i <= 10; i++){ 
+        console.log(`${number} x ${i} é: ${number * i}`)
+    }
+}else{
+    console.log('Por favor, digite um número entre 1 e 10: ')
+}
