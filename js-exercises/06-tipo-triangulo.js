74f3785aa0ae835c42f30239e215f54e0f338b48
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 17:18:56 -0300

    feat: add tipo-triangulo exercise

--- js-exercises/06-tipo-triangulo.js
+++ js-exercises/06-tipo-triangulo.js
@@ -4,3 +4,28 @@
 // Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B = C)
 // Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C)
 // Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
+
+const prompt = require('prompt-sync')()
+
+let ladoA = Number(prompt('Digite um número inteiro para o lado A do triângulo: ')) 
+let ladoB = Number(prompt('Digite um número inteiro para o lado B do triângulo: '))
+let ladoC = Number(prompt('Digite um número inteiro para o lado C do triângulo: '))
+
+if((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB)){
+    console.log('Os lados fornecidos formam um triângulo!')
+
+    if((ladoA == ladoB) && (ladoB == ladoC)){
+        console.log('Todos os lados iguais formam um Triângulo Equilátero!')
+    }else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)){
+        console.log('Os dois lados iguais formam um Triângulo Isósceles!')
+    }else{
+        console.log('Todos os lados diferentes formam um Triângulo Escaleno!')
+    }
+
+}else{
+    console.log('Os lados fornecidos não formam um triângulo!')
+}
+
+
+
+
