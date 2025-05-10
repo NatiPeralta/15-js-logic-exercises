21b4921877e45153607bdd87211c101cb8612e62
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 19:45:51 -0300

    feat: add repeticao-numero exercise

--- js-exercises/10-repeticao-numero.js
+++ js-exercises/10-repeticao-numero.js
@@ -1,1 +1,11 @@
 //10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+
+const prompt = require('prompt-sync')()
+let number = Number(prompt('Digite um número inteiro: '))
+
+for(let numb = 1; numb <= 10; numb++){
+    console.log(`${numb}ª vez: ${number}`)
+}
+
+console.log('Repetição finalizada!')
+
