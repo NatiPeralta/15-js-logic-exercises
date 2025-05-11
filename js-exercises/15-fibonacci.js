b9f3ee90f482b17a83239d0edbb833c27ebd3b23
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 23:33:48 -0300

    feat: add fibonacci exercise

--- js-exercises/15-fibonacci.js
+++ js-exercises/15-fibonacci.js
@@ -1,1 +1,16 @@
 //15. Escreva um programa que gera e imprime os primeiros 10 números da sequência de Fibonacci utilizando um loop for.
+
+const prompt = require('prompt-sync')()
+
+let repeat = Number(prompt('Quantas vezes você deseja ver a sequência de Fibonacci? '));
+let number1 = 0
+let number2 = 1
+
+console.log('Sequência de Fibonacci: ');
+
+for(i = 1; i <= repeat; i++){
+    console.log(number1)
+    let proximo = number1 + number2;
+    number1 = number2;
+    number2 = proximo;
+}
