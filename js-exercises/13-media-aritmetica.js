46e4439a1454cf61d42a41c1e56bd8baf4303b7b
Natália Guedes Peralta <nati.g.peralta@hotmail.com>
2025-05-10 22:07:36 -0300

    feat: add media-aritmetica exercise

--- js-exercises/13-media-aritmetica.js
+++ js-exercises/13-media-aritmetica.js
@@ -1,1 +1,24 @@
 //13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer a média aritmética desses números.
+
+const prompt = require('prompt-sync')()
+
+let soma = 0
+let cont = 0
+let result = 0
+
+do{
+    result = parseFloat(prompt('Digite números decimais (Para finalizar a operação e calcular a média, digite 0): '))
+
+    if(result !== 0){
+        soma += result //adiciona o numero a soma total
+        cont++ //incrementa a contagem dos numeros
+    }
+
+}while(result != 0)
+
+    if(cont > 0){
+        let media = soma/(cont)
+        console.log(`A média final é: ${media.toFixed(2)}`)
+    }else{
+        console.log('Digite um número válido, por favor.')
+    }
